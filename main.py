ca624ae3028ba53a7a07cae8b8b623f45db83f9b
Solibex <zsolibex@gmail.com>
2021-11-10 20:23:53 +0700

    removed update function useless add update function code into main.loop()

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ class main:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-    def update():
-        pygame.display.flip()
+        pygame.display.update()
 
 
 class images:
@@ -25,6 +24,6 @@ pygame.display.set_caption(data.caption)
 pygame.display.set_icon(images.icon)
 screen.fill((255, 255, 255))
 
+
 while True:
     main.loop()
-    main.update()
