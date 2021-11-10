da33069c8fd04825ae02e2d785e2b32d4166230f
Solibex <zsolibex@gmail.com>
2021-11-10 20:08:52 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+class main:
+    def loop():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+class images:
+    icon = pygame.image.load('data/gfx/logo.png')
+class data:
+    caption = 'smile yez'
+    canvas = (500,500)
+screen = pygame.display.set_mode(data.canvas)
+pygame.display.set_caption(data.caption)
+pygame.display.set_icon(images.icon)
+
+while True:
+    main.loop()
