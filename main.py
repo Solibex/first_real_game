59d2a5e8ebc5b9c350ac6526e4701872f1b8bf16
Solibex <zsolibex@gmail.com>
2021-11-10 20:10:23 +0700

    added player class

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import pygame
 
 pygame.init()
 
+
 class main:
     def loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
+
 class images:
     icon = pygame.image.load('data/gfx/logo.png')
+
+
 class data:
     caption = 'smile yez'
-    canvas = (500,500)
+    canvas = (500, 500)
+class player:
+    image =
+
 screen = pygame.display.set_mode(data.canvas)
 pygame.display.set_caption(data.caption)
 pygame.display.set_icon(images.icon)
